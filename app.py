e2378f5e89f4be3736b30e354bd1828f03d5e663
jbrowning24 <166178738+jbrowning24@users.noreply.github.com>
2026-01-13 23:45:04 -0500

    Use 5000 as default Flask port

--- app.py
+++ app.py
@@ -202,5 +202,5 @@ def index():
     return render_template("index.html", result=result, error=error)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8000"))
+    port = int(os.environ.get("PORT", "5000"))
     app.run(host="0.0.0.0", port=port, debug=True)
